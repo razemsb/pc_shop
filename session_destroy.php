169382f25dd7255e395472232d2e896d2d109e28
raze <155824568+razemsb@users.noreply.github.com>
2024-11-01 22:19:46 +0300

    Update session_destroy.php

---
+++ session_destroy.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if (isset($_SESSION['user_login'])) {
+    unset($_SESSION['user_login']);
+    
+    session_unset();
+    session_destroy();
+    if(isset($_SESSION['user_Login'])) {
+
+    echo "<script>alert('Вы вышли из аккаунта.')</script>";
+    header("Location: page.php");
+    exit();
+    }else {
+    header("Location: page.php");   
+    exit();
+    }
+} else {
+    header("Location: page.php");
+    exit();
+}
+?>
